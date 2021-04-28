cbfb0665bf2696f8cd422d8aaabc0078c51c55e9
theremotecreative <jason@theremotecreative.com>
2021-04-28 13:38:28 -0500

    contact page v2

--- src/pages/contact/contact-us.js
+++ src/pages/contact/contact-us.js
@@ -1,11 +1,22 @@
 import React from "react"
 import { useStaticQuery, graphql } from 'gatsby'
 import styled from 'styled-components'
+import Slider from "react-slick"
 import Img from "gatsby-image"
 
 import Layout from "../../components/layout-v2"
 
 const ContactUsPage = () => {
+
+    const settings = {
+      arrows: false,
+      dots: false,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      focusOnSelect: true,
+      centerMode: true,
+    }
 
     const data = useStaticQuery(graphql`
       query {
@@ -99,13 +110,17 @@ const ContactUsPage = () => {
                     </div>
                 </SectionThree>
                 <SectionFour>
-                    {post.node.ContactUsContent.contactScrollImages.map(image => (
+                  <div class="contact-slider">
+                    <Slider {...settings}>
+                      {post.node.ContactUsContent.contactScrollImages.map(image => (
                         <Img fluid={image.localFile.childImageSharp.fluid} alt={"scroll image"} />
-                    ))}
-                    <div class="phone-content">
-                        <Img fluid={data.phoneImage.childImageSharp.fixed} />
-                        <p>888.689.0872</p>
-                    </div>
+                      ))}
+                    </Slider>
+                  </div>
+                  <div class="phone-content">
+                      <Img fluid={data.phoneImage.childImageSharp.fixed} />
+                      <p>888.689.0872</p>
+                  </div>
                 </SectionFour>
             </Layout>
         ))
@@ -128,13 +143,128 @@ const SectionOne = styled.section`
 const SectionTwo = styled.section`
     width: 100%;
     position: relative;
-
+    overflow: hidden;
+    padding: 100px 0;
+    .background-image {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      &:after {
+        content: '';
+        background-color: rgba(0,0,0,.3);
+        position: absolute;
+        height: 100%;
+        width: 100%;
+        top: 0;
+        left: 0;
+      }
+      .gatsby-image-wrapper {
+        height: 100%;
+      }
+    }
+    .section-content {
+      position: relative;
+      z-index: 10;
+      padding: 0 20px;
+      p {
+        font-family: "Kessel Light";
+        line-height: 1.8;
+        font-size: 14px;
+        color: #fff;
+        font-weight: 400;
+        text-align: center;
+        margin-bottom: 0;
+      }
+    }
 `
 
 const SectionThree = styled.section`
-
+    width: 100%;
+    padding: 50px 20px;
+    h2 {
+      font-family: "Kessel Light";
+      font-size: 60px;
+      color: #fff;
+      font-weight: 400;
+      text-align: center;
+      margin-top: 0;
+      margin-bottom: 35px;
+      text-transform: uppercase;
+    }
+    .location-flex {
+      max-width: 1000px;
+      width: 100%;
+      display: flex;
+      margin: 0 auto;
+      > div {
+        width: 33.33%;
+        text-align: center;
+        .gatsby-image-wrapper {
+          width: 35px;
+          height: 50px;
+          margin: 0 auto;
+        }
+        h3 {
+          font-family: "Madelyn";
+          font-size: 85px;
+          font-weight: normal;
+          line-height: 35px;
+          color: #d2232a;
+          text-align: center;
+        }
+        p {
+          font-family: "Kessel Light";
+          line-height: 1.8;
+          font-size: 14px;
+          color: #fff;
+          font-weight: 400;
+          text-align: center;
+          margin-bottom: 0;
+        }
+        .location-content {
+          margin-top: -30px;
+        }
+      }
+    }
 `
 
-const SectionFour = styled.section``
+const SectionFour = styled.section`
+  width: 100%;
+  .contact-slider {
+    width: 100%;
+    max-width: 100%;
+    height: 350px;
+    margin: 0 auto;
+    .slick-slider {
+      max-width: 525px;
+      width: 100%;
+      margin: 0 auto;
+      overflow: visible;
+    }
+    .slick-list{
+      padding: 0;
+      overflow: visible;
+    }
+    .slick-slide {
+      width: 100%;
+      transition-duration: .3s;
+      outline: 0 !important;
+      height: 350px;
+      width: 525px;
+      > div {
+        height: 350px;
+        width: 525px;
+        border: 2px solid #d2232a;
+      }
+      .gatsby-image-wrapper {
+        height: 350px;
+        width: 525px;
+        width: 100%;
+      }
+    }
+  }
+`
 
 export default ContactUsPage
