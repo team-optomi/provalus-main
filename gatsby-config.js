af84ba672081ebc63dae3557395c9564380f0f90
theremotecreative <jason@theremotecreative.com>
2021-08-02 14:56:13 -0500

    gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -72,6 +72,15 @@ module.exports = {
     `gatsby-plugin-sharp`,
     `gatsby-plugin-netlify`,
     {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "UA-102657039-1", // Google Analytics / GA
+        ],
+      },
+    },
+    {
       // See https://www.gatsbyjs.com/plugins/gatsby-plugin-manifest/?=gatsby-plugin-manifest
       resolve: `gatsby-plugin-manifest`,
       options: {
