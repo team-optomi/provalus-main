3a7344ef792e47a635dfd5b207c8e53534dfdc6e
theremotecreative <jason@theremotecreative.com>
2023-05-19 12:17:06 -0600

    client value slider

--- src/pages/company/client-value.js
+++ src/pages/company/client-value.js
@@ -1,12 +1,36 @@
 import React from "react"
-import { useStaticQuery, graphql } from 'gatsby'
+import { useStaticQuery, graphql, Link } from 'gatsby'
 import styled from 'styled-components'
+import Slider from "react-slick"
 import Img from "gatsby-image"
 
 import Layout from "../../components/layout-v2"
 import SEO from "../../components/seo"
 
 const ClientValuePage = () => {
+
+    const settings = {
+      arrows: true,
+      dots: false,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: false,
+      responsive: [
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 2,
+          }
+        },
+        {
+          breakpoint: 500,
+          settings: {
+            slidesToShow: 1,
+          }
+        }
+      ]
+    };
 
     const data = useStaticQuery(graphql`
       query {
@@ -162,6 +186,24 @@ const ClientValuePage = () => {
                   }
                   colTwelveCopy
                 }
+                clientValueSectionTitle
+                clientValueSectionBlogPosts {
+                  ... on WpPost {
+                    title
+                    slug
+                    featuredImage {
+                      node {
+                        localFile {
+                          childImageSharp {
+                            fluid(maxWidth: 2467) {
+                                ...GatsbyImageSharpFluid
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
                 sectionTwo {
                     sectionCopy
                     sectionImage {
@@ -250,6 +292,21 @@ const ClientValuePage = () => {
                     <p dangerouslySetInnerHTML={{ __html: post.node.ClientValueContent.sectionOne.colTwelveCopy }} />
                 </IconCol>
             </IconSection>
+            <ValueSection>
+              <h2>{post.node.ClientValueContent.clientValueSectionTitle}</h2>
+              <div class="blog-loop">
+                <Slider {...settings}>
+                {post.node.ClientValueContent.clientValueSectionBlogPosts.map(insightPost => (
+                  <div class="blog-slide">
+                    <Link to={`/insights/${insightPost.slug}`}>
+                      <Img fluid={insightPost.featuredImage.node.localFile.childImageSharp.fluid} alt={insightPost.title} />
+                      <h3>{insightPost.title}</h3>
+                    </Link>
+                  </div>
+                ))}
+                </Slider>
+              </div>
+            </ValueSection>
             <SectionContent 
             data-sal="fade"
             data-sal-duration="1000"
@@ -271,8 +328,7 @@ const ClientValuePage = () => {
   
   
   const PageMain = styled.div`
-    width: 100%;
-    padding: 200px;
+    padding: 200px 0;
     h1 {
         font-family: "Madelyn";
         font-size: 185px;
@@ -282,10 +338,6 @@ const ClientValuePage = () => {
         color: #d2232a;
         text-align: center;
     }
-    @media(max-width:1000px) {
-      padding: 200px 20px;
-      padding-bottom: 50px;
-    }
     @media(max-width:850px) {
       padding-top: 100px;
       h1 {
@@ -294,7 +346,117 @@ const ClientValuePage = () => {
     }
   `
 
+  const ValueSection = styled.section`
+    h2 {
+      font-family: "Madelyn";
+      font-size: 100px;
+      font-weight: normal;
+      text-transform: lowercase;
+      line-height: 35px;
+      color: #d2232a;
+      text-align: center;
+    }
+    .blog-loop {
+      height: 350px;
+      .slick-prev {
+        width: 40px;
+        height: 40px;
+        border-top: 7px solid #fff;
+        border-left: 7px solid #fff;
+        left: 20px;
+        top: calc(50% - 20px);
+        transform: rotate(-45deg);
+        opacity: .7;
+        transition-duration: .3s;
+        z-index: 10;
+        &:before {
+          display: none;
+        }
+        &:hover {
+          opacity: 1;
+        }
+      }
+      .slick-next {
+        width: 40px;
+        height: 40px;
+        border-top: 7px solid #fff;
+        border-right: 7px solid #fff;
+        right: 20px;
+        top: calc(50% - 20px);
+        transform: rotate(45deg);
+        opacity: .7;
+        transition-duration: .3s;
+        z-index: 10;
+        &:before {
+          display: none;
+        }
+        &:hover {
+          opacity: 1;
+        }
+      }
+      .blog-slide {
+        position: relative;
+        height: 350px;
+        a {
+          display: block;
+          height: 100%;
+          width: 100%;
+          text-decoration: none;
+        }
+        .gatsby-image-wrapper {
+          position: absolute;
+          width: 100%;
+          height: 100%;
+          img {
+            width: 100%;
+            height: 100%;
+            object-fit: cover;
+          }
+          &:after {
+            content: '';
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: rgba(0,0,0,.3);
+            transition-duration: 0.3s;
+          }
+        }
+        h3 {
+          position: absolute;
+          left: 0;
+          bottom: 0;
+          width: 100%;
+          padding: 15px;
+          color: #fff;
+          margin: 0;
+          z-index: 2;
+        }
+        &:hover {
+          .gatsby-image-wrapper {
+            &:after {
+              background-color: rgba(210, 35, 42,.3);
+            }
+          }
+        }
+      }
+    }
+    @media(max-width:767px) {
+      .blog-loop {
+        .slick-prev {
+          display: none !important;
+        }
+        .slick-next {
+          display: none !important;
+        }
+      }
+    }
+  `
+
   const SectionContent = styled.section`
+    width: 100%;
+    padding: 0 200px;
     p {
         font-family: "Kessel Light";
         padding-top: 30px;
@@ -307,6 +469,10 @@ const ClientValuePage = () => {
             display: none;
           }
         }
+    }
+    @media(max-width:1000px) {
+      padding: 0 20px;
+      padding-bottom: 50px;
     }
     @media(max-width:767px) {
       h2 {
@@ -323,11 +489,17 @@ const ClientValuePage = () => {
 
   const IconSection = styled.section`
     max-width: 1400px;
+    margin: 0 auto;
+    padding: 0 200px;
     width: 100%;
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
     align-items: center;
+    @media(max-width:1000px) {
+      padding: 0 20px;
+      padding-bottom: 50px;
+    }
     @media(max-width:540px) {
       opacity: 1 !important;
     }
